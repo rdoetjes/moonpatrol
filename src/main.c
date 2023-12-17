6dabe288fbba5d269dc9c4fe29d3d48c806e6c27
Raymond Doetjes <rdoetjes@phonax.com>
2023-12-17 23:04:26 +0100

    moved the 3 game functions to the top for easier access

--- src/main.c
+++ src/main.c
@@ -15,6 +15,26 @@ void setup(void){
 }
 
 /*
+Draw everything
+*/
+void draw(){
+    char lives[12];
+    VDP_setHorizontalScroll(BG_B, scroll_bg_b_offset);
+    sprintf(lives, "LIVES: %d", p1.lives);
+    VDP_drawText(lives, 40-strlen(lives)-2, 1);
+}
+
+/*
+Process game logic
+*/
+void logic(){
+    frame_count++; 
+    if ( (frame_count & 3) == 0) {
+        scroll_bg_b_offset--;
+    }
+}
+
+/*
 sets up the player pointed to by *player
 The number of lives is externally configurable, as that might depend on hard or easy game.
 The rest of the Player struct is set with fixed values that work for the game.
@@ -36,26 +56,6 @@ void bg_b(void){
     VDP_drawImageEx(BG_B, &background, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, idx), 0, -3, FALSE, TRUE);
     idx += background.tileset->numTile;
     VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);
-}
-
-/*
-Draw everything
-*/
-void draw(){
-    char lives[12];
-    VDP_setHorizontalScroll(BG_B, scroll_bg_b_offset);
-    sprintf(lives, "LIVES: %d", p1.lives);
-    VDP_drawText(lives, 40-strlen(lives)-2, 1);
-}
-
-/*
-Process game logic
-*/
-void logic(){
-    frame_count++; 
-    if ( (frame_count & 3) == 0) {
-        scroll_bg_b_offset--;
-    }
 }
 
 /*
