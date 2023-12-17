a168328e1d4e751f6adfb276b152fd3b571d9bfe
Raymond Doetjes <rdoetjes@phonax.com>
2023-12-17 22:25:40 +0100

    refactor and introduction of Player struct

--- src/main.c
+++ src/main.c
@@ -1,24 +1,51 @@
-#include <genesis.h>
+#include "main.h"
 #include <resources.h>
+#include <string.h>
 
-u16 idx = TILE_USER_INDEX;
+static s16 scroll_bg_b_offset = 0;
+static Player p1;
+static u8 frame_count;
 
-int main()
-{   
+void setup(void){
+    bg_b();
+    setup_player(&p1, 3);
+}
+
+void setup_player(Player *player, const int lives){
+    player->lives = lives;
+    player->y = 220;
+    player->x = 110;
+}
+
+void bg_b(void){
+    u16 idx = TILE_USER_INDEX;
     PAL_setPalette(PAL0, background.palette->data, DMA);
     VDP_drawImageEx(BG_B, &background, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, idx), 0, -3, FALSE, TRUE);
     idx += background.tileset->numTile;
     VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);
+}
 
-    s16 i = 0;
-    u16 frame_count;
+void draw(){
+    char lives[12];
+    VDP_setHorizontalScroll(BG_B, scroll_bg_b_offset);
+    sprintf(lives, "LIVES: %d", p1.lives);
+    VDP_drawText(lives, 40-strlen(lives)-2, 1);
+}
+
+void logic(){
+    frame_count++; 
+    if ( (frame_count & 3) == 0) {
+        scroll_bg_b_offset--;
+    }
+}
+
+int main()
+{   
+    setup();
     while(1)
     {
-        frame_count++; 
-        if (frame_count % 4 == 0) {
-            i--;
-        }
-        VDP_setHorizontalScroll(BG_B, i);
+        logic();
+        draw();
         SYS_doVBlankProcess();
     }
     return (0);
---
+++ src/main.h
@@ -0,0 +1,13 @@
+#include <genesis.h>
+
+typedef struct Player{
+    u8 lives;
+    u16 x;
+    u16 y;
+} Player;
+
+void setup(void);
+void bg_b(void);
+void draw(void);
+void logic(void);
+void setup_player(Player *player, const int lives);
