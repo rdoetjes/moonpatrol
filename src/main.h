505c1e9237d42e0a8e001343630ddf7a0e72fd90
Raymond Doetjes <rdoetjes@phonax.com>
2023-12-21 12:13:52 +0100

    missed inline in main.h

--- src/main.h
+++ src/main.h
@@ -13,7 +13,7 @@ typedef struct Player{
 static void setup(void);
 static void show_game_bg_b(void);
 static void show_gameover_bg_b();
-static void load_vdp(u16 numPal, const u16* pal, int x, int y, VDPPlane plane, const Image *image);
+inline static void load_vdp(u16 numPal, const u16* pal, int x, int y, VDPPlane plane, const Image *image);
 static void draw();
 static void logic(void);
 static void setup_player(Player *player, const u8 lives);
