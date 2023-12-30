ed4389cc0bfa457f91f443bc297257af3bb5ac77
Raymond Doetjes <rdoetjes@phonax.com>
2023-12-30 18:59:01 +0100

    just clean ordering in code

--- src/setup.c
+++ src/setup.c
@@ -5,8 +5,10 @@
 Sets up the game's game_bg_b, sprites, player and other required initializations.
 */
 void setup(Player* p1){
+    setup_player(p1, 3);   // setup player 1 with 3 lives (currently we only have a player 1)
+
     show_game_bg_b();
-    setup_player(p1, 3);
     scroll_bg_b_offset = 0;
+    
     JOY_init();
 }
