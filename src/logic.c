f13bec61140c6428b32566f30bc802d02ff1c34c
Raymond Doetjes <rdoetjes@phonax.com>
2024-01-01 15:02:39 +0100

    save nested if

--- src/logic.c
+++ src/logic.c
@@ -4,12 +4,12 @@
 u16 process_joy(){
     u16 input = JOY_readJoypad(JOY_1);
 
-    if(input & BUTTON_LEFT) {
-        if (p1.x > LEFT_BOUNDERY_PLAYER) p1.x-=2;              
+    if(input & BUTTON_LEFT && p1.x > LEFT_BOUNDERY_PLAYER){
+        p1.x-=2;              
     }
 
-    if(input & BUTTON_RIGHT) {
-        if (p1.x < RIGHT_BOUNDERY_PLAYER) p1.x+=2;        
+    if(input & BUTTON_RIGHT && p1.x < RIGHT_BOUNDERY_PLAYER) {
+        p1.x+=2;        
     }
 
     return input;
