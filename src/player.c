30e0d569a14ad8afaa62cf55102c4545d4bdf11d
Raymond Doetjes <rdoetjes@phonax.com>
2024-01-01 14:23:40 +0100

    start position player from struct

--- src/player.c
+++ src/player.c
@@ -2,8 +2,8 @@
 #include "globals.h"
 #include "resources.h"
 
-static void load_sprite(void){
-    p1_sprite = SPR_addSprite(&player_sprite, 100, 180, TILE_ATTR(PAL2, FALSE, FALSE, FALSE));
+static void load_sprite(Player* player){
+    p1_sprite = SPR_addSprite(&player_sprite, player->x, player->y, TILE_ATTR(PAL2, FALSE, FALSE, FALSE));
     PAL_setPalette(PAL2, player_sprite.palette->data, DMA);
 }
 
@@ -13,12 +13,12 @@ The number of lives is externally configurable, as that might depend on hard or 
 The rest of the Player struct is set with fixed values that work for the game.
 */
 void setup_player(Player *player, const u8 lives){
-    player->lives = lives;
-    player->y = 220;
-    player->x = 110;
+    player->lives = lives;    
+    player->x = 90;
+    player->y = 180;
     player->isJumping = FALSE;
     player->score = 0;
 
-    load_sprite();
+    load_sprite(player);
     SPR_setAnim(p1_sprite, 0);
 }
