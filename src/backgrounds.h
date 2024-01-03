025af855afa0ea7e3a87352428ee2e54dd3a7e02
Raymond Doetjes <rdoetjes@phonax.com>
2024-01-03 12:28:45 +0100

    parmeterization

--- src/backgrounds.h
+++ src/backgrounds.h
@@ -3,3 +3,4 @@
 
 void show_game_bg_b(void);
 void show_gameover_bg_b(void);
+void show_sega_bg_a(void);
--- src/setup.c
+++ src/setup.c
@@ -9,10 +9,7 @@ Sets up the game's game_bg_b, sprites, player and other required initializations
 */
 void setup(Player* p1){
     SPR_init();
-    JOY_init();
-
+    setup_player(p1, 3);   // setup player 1 with 3 lives (currently we only have a player 1)
     show_game_bg_b();
     scroll_bg_b_offset = 0;    
-
-    setup_player(p1, 3);   // setup player 1 with 3 lives (currently we only have a player 1)
 }
