895e848b191ff794dce4e17a9b11f8b72fe368a4
Raymond Doetjes <rdoetjes@phonax.com>
2024-01-03 12:30:19 +0100

    param fixed

--- src/backgrounds.c
+++ src/backgrounds.c
@@ -9,8 +9,8 @@ of doing those lines for every screen we want to load.
 */
 inline static void load_vdp(u16 numPal, const u16* pal, int x, int y, VDPPlane plane, const Image *image){
     u16 idx = TILE_USER_INDEX;
-    PAL_setPalette(PAL0, image->palette->data, DMA);
-    VDP_drawImageEx(BG_B, image, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, idx), x, y, FALSE, TRUE);
+    PAL_setPalette(numPal, image->palette->data, DMA);
+    VDP_drawImageEx(plane, image, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, idx), x, y, FALSE, TRUE);
     idx += image->tileset->numTile;
 }
 
